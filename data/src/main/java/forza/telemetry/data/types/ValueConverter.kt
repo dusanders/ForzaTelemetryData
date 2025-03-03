e1649dfbb92d77a8c37670fab33a6cdd407ec658
dusanders <dusanders@gmail.com>
2025-03-03 11:16:35 -0600

    Update ValueConverter.kt
    move to extension methods

--- data/src/main/java/forza/telemetry/data/types/ValueConverter.kt
+++ data/src/main/java/forza/telemetry/data/types/ValueConverter.kt
@@ -1,16 +1,14 @@
 package forza.telemetry.data.types
 
-class ValueConverter {
-    fun speedToMPH(speed: Float): Float {
-        return speed * 2.23694f
-    }
-    fun speedToKPH(speed: Float): Float {
-        return speed * 3.6f
-    }
-    fun powerToHorsepower(power: Float): Float {
-        return power * 0.00134102f
-    }
-    fun tempToCelsius(temp: Float): Float {
-        return (((temp - 32) * 5) / 9)
-    }
+fun Float.toMPH(): Float {
+    return this * 2.23694f
+}
+fun Float.toKPH(): Float {
+    return this * 3.6f
+}
+fun Float.toHorsepower(): Float {
+    return this * 0.00134102f
+}
+fun Float.toCelsius(): Float {
+    return (((this - 32) * 5) / 9)
 }
